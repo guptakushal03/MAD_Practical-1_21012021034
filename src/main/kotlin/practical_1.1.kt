138dc8ffa39125636852f4af2afcac80c35411c4
student <student@gnu.ac.in>
2023-07-17 10:39:09 +0530

    Practical 1

--- src/main/kotlin/practical_1.1.kt
+++ src/main/kotlin/practical_1.1.kt
@@ -1,3 +1,14 @@
 fun main(){
-    println("Hello, World!")
+    var a : Int = 5
+    var b : Float = 5.55f
+    var c : Char = 'a'
+    var d : String = "KG"
+    var e : Boolean = false
+    var f : Double = 5.05
+    var g : Long = 5
+    var h : Short = 5
+    var i : Byte = 5
+
+
+    println("$a, $b, $c, $d, $e, $f, $g, $h, $i")
 }
---
+++ src/main/kotlin/practical_1.2.kt
@@ -0,0 +1,12 @@
+import kotlin.reflect.typeOf
+
+fun main(){
+    var a : Int = 5
+    a.toString()
+    print("$a as String")
+
+    a.toDouble()
+    print("$a as Double")
+
+    println("$a")
+}
